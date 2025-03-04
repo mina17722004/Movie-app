9e53aa00ecd37fe64c2264c0b7fc4ea5af6fda46
hp <minanematalla10@gmail.com>
2025-03-04 21:41:22 +0200

    inital commit

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -12,13 +12,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
   int _selectedTab = 0;
 
-  final List<String> movieImages = [
-    AppAssets.superMan,
-    AppAssets.doctorStrange,
-    AppAssets.film,
+  final List<Widget> _screens = [
+    HomeWidget(),
+    SearchWidget(),
+    ExploreWidget(),
+    ProfileWidget(),
   ];
 
   @override
@@ -26,8 +26,8 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: appColors.black,
       bottomNavigationBar: SalomonBottomBar(
-       itemPadding: EdgeInsets.all(16),
-        //backgroundColor: appColors.grey,
+        itemPadding: EdgeInsets.all(8),
+        margin: EdgeInsets.all(12),
         currentIndex: _selectedTab,
         onTap: (index) {
           setState(() {
@@ -39,130 +39,152 @@ class _HomeScreenState extends State<HomeScreen> {
           SalomonBottomBarItem(
             icon: Icon(Icons.home, size: 28),
             title: Text("Home"),
-              selectedColor: appColors.yellow,
-            unselectedColor: appColors.white
+            selectedColor: appColors.yellow,
+            unselectedColor: appColors.white,
           ),
           SalomonBottomBarItem(
             icon: Icon(Icons.search, size: 28),
             title: Text("Search"),
-              selectedColor: appColors.yellow,
-              unselectedColor: appColors.white
+            selectedColor: appColors.yellow,
+            unselectedColor: appColors.white,
           ),
           SalomonBottomBarItem(
             icon: Icon(Icons.explore, size: 28),
             title: Text("Explore"),
-              selectedColor: appColors.yellow,
-              unselectedColor: appColors.white
+            selectedColor: appColors.yellow,
+            unselectedColor: appColors.white,
           ),
           SalomonBottomBarItem(
             icon: Icon(Icons.person, size: 28),
             title: Text("Profile"),
             selectedColor: appColors.yellow,
-              unselectedColor: appColors.white
+            unselectedColor: appColors.white,
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          // Dynamic Background Image
-          Positioned.fill(
-            child: AnimatedSwitcher(
-              duration: Duration(milliseconds: 500),
-              child: Image.asset(
-                movieImages[_currentIndex],
-                key: ValueKey<String>(movieImages[_currentIndex]),
-                fit: BoxFit.cover,
-                color: Colors.black.withOpacity(0.5),
-                colorBlendMode: BlendMode.darken,
+      body: _screens[_selectedTab],
+    );
+  }
+}
+
+// Home Widget
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  int _currentIndex = 0;
+  final List<String> movieImages = [
+    AppAssets.superMan,
+    AppAssets.doctorStrange,
+    AppAssets.film,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+
+        Positioned.fill(
+          child: AnimatedSwitcher(
+            duration: Duration(milliseconds: 200),
+            child: Container(
+              key: ValueKey<String>(movieImages[_currentIndex]),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(movieImages[_currentIndex]),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.darken,
+                  ),
+                ),
               ),
             ),
           ),
+        ),
 
-          Positioned(
-            top: 10,
-            left: MediaQuery.of(context).size.width * 0.28,
-            child: Image.asset(AppAssets.avaliableNow, width: 200),
-          ),
+        Positioned(
+          top: 20,
+          left: MediaQuery.of(context).size.width*0.28,
+          child: Image.asset(AppAssets.avaliableNow, width: 200),
+        ),
 
-          // Movie Posters Carousel
-          Positioned(
-            top: 70,
-            left: 0,
-            right: 0,
-            height: 260,
-            child: CarouselSlider.builder(
-              itemCount: movieImages.length,
-              options: CarouselOptions(
-                height: 250,
-                autoPlay: true,
-                enlargeCenterPage: true,
-                viewportFraction: 0.5,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-              itemBuilder: (context, index, realIndex) {
-                return moviePoster(movieImages[index]);
+        Positioned(
+          top: 90,
+          left: 0,
+          right: 0,
+          height: 350,
+          child: CarouselSlider.builder(
+            itemCount: movieImages.length,
+            options: CarouselOptions(
+              height: 350,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              viewportFraction: 0.5,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
               },
             ),
+            itemBuilder: (context, index, realIndex) {
+              return moviePoster(movieImages[index]);
+            },
           ),
-
-          // "Watch Now"
-          Positioned(
-            top: 330,
-            left: MediaQuery.of(context).size.width * 0.28,
-            child: Image.asset(AppAssets.watchNow, width: 200),
+        ),
+        Positioned(
+          top: 440,
+          left: MediaQuery.of(context).size.width * 0.28,
+          child: Image.asset(AppAssets.watchNow, width: 200),
+        ),
+        // Action Movies Section (Updated ListView)
+        Positioned(
+          top: 520,
+          left: 20,
+          child: Text(
+            "Action",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+        ),
 
-          // Action Movies Section
-          Positioned(
-            top: 450,
-            left: 20,
+        Positioned(
+          top: 520,
+          right: 20,
+          child: GestureDetector(
+            onTap: () {
+              // Handle "See More"
+            },
             child: Text(
-              "Action",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+              "See More →",
+              style: TextStyle(color: Colors.yellow, fontSize: 14),
             ),
           ),
+        ),
 
-          Positioned(
-            top: 450,
-            right: 20,
-            child: GestureDetector(
-              onTap: () {
-                // Handle "See More"
-              },
-              child: Text(
-                "See More →",
-                style: TextStyle(color: Colors.yellow, fontSize: 14),
-              ),
-            ),
+        // Horizontal Movie List (Updated to ensure valid images)
+        Positioned(
+          top: 550,
+          left: 0,
+          right: 0,
+          height: 170,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              movieThumbnail(AppAssets.openHimer),
+              movieThumbnail(AppAssets.doctorStrange),
+              movieThumbnail(AppAssets.superMan),
+              movieThumbnail(AppAssets.film),
+            ],
           ),
-
-          // Horizontal Movie List
-          Positioned(
-            top: 480,
-            left: 0,
-            right: 0,
-            height: 170,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                movieThumbnail(AppAssets.doctorStrange),
-                movieThumbnail(AppAssets.superMan),
-                movieThumbnail(AppAssets.film),
-                movieThumbnail(AppAssets.openHimer),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -197,6 +219,36 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+// Search Widget
+class SearchWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Search Screen", style: TextStyle(color: Colors.white, fontSize: 24)),
+    );
+  }
+}
+
+// Explore Widget
+class ExploreWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Explore Screen", style: TextStyle(color: Colors.white, fontSize: 24)),
+    );
+  }
+}
+
+// Profile Widget
+class ProfileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Profile Screen", style: TextStyle(color: Colors.white, fontSize: 24)),
     );
   }
 }
